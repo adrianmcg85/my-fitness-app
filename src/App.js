9b4e22327cc2960af545d2c4301c9e88ea62b2bb
adrianmcg85 <adrian.m.mcg@gmail.com>
2020-04-16 22:49:42 +0100

    stats component created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Stats from './components/stats';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+    <Stats />
     </div>
   );
 }
